c9a90eb493ea0da815fe9ecb59aac04f58fa0f1a
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-10-05 22:03:42 +0300

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -27,12 +27,12 @@ def canUnlockAll(boxes):
 
 
 # Example usage:
-if __name__ == "__main__":
-    boxes = [[1], [2], [3], [4], []]
-    print(canUnlockAll(boxes))  # True
+# if __name__ == "__main__":
+#     boxes = [[1], [2], [3], [4], []]
+#     print(canUnlockAll(boxes))  # True
 
-    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-    print(canUnlockAll(boxes))  # True
+#     boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+#     print(canUnlockAll(boxes))  # True
 
-    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(canUnlockAll(boxes))  # False
+#     boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+#     print(canUnlockAll(boxes))  # False
