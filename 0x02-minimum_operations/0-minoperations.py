c0bcb12ea3a458d97c7db9ccae2ef048d8de13df
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-10-10 15:26:06 +0300

    0-minoperations.py - 0. Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,46 +2,31 @@
 """Minimum Operations"""
 
 
-def minOperations(n: int) -> int:
+def minOperations(n):
     """
-    Calculate the fewest number of operations needed to result in exactly n H
-    characters in the file.
+    Calculate the fewest number of operations needed to reach 'n' 'H'
+      characters.
 
     Args:
-    - n (int): The target number of characters.
+        n (int): The target number of 'H' characters.
 
     Returns:
-    - int: The minimum number of operations required.
+        int: The minimum number of operations needed to reach 'n' 'H'
+          characters.
 
-    If n is impossible to achieve, return 0.
     """
     if n <= 1:
         return 0
 
-    op = 0
-    body = 'H'
-    next_char = 'H'
+    operations = 0
+    clipboard = 1
+    total = 1
 
-    for i in range(2, n + 1):
-        if n % len(body) == 0:
-            op += 2
-            next_char = body
-            body += body
-        else:
-            op += 1
-            body += next_char
+    for _ in range(n):
+        if n % total == 0:
+            clipboard = total
+            operations += 1
+        operations += 1
+        total += clipboard
 
-    if len(body) != n:
-        return 0
-
-    return op
-
-
-if __name__ == "__main__":
-    n = 4
-    print("Min # of operations to reach {} char: {}".
-          format(n, minOperations(n)))
-
-    n = 12
-    print("Min # of operations to reach {} char: {}".
-          format(n, minOperations(n)))
+    return operations
