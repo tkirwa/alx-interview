87c7e46b917393f4a6a3388320a631ea8a9a2d4b
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-10-31 17:36:09 +0300

    0-nqueens.py - write a program that solves the N queens problem

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,99 +1,68 @@
 #!/usr/bin/python3
-
-""" The N queens puzzle is the challenge of placing N non-attacking
- queens on an N×N chessboard
+"""
+Solution to the nqueens problem
 """
 import sys
 
 
-def solve_nqueens(n):
+def backtrack(r, n, cols, pos, neg, board):
     """
-    Solve the N queens problem using backtracking.
+    backtrack function to find solution
+    """
+    if r == n:
+        res = []
+        for l in range(len(board)):
+            for k in range(len(board[l])):
+                if board[l][k] == 1:
+                    res.append([l, k])
+        print(res)
+        return
 
+    for c in range(n):
+        if c in cols or (r + c) in pos or (r - c) in neg:
+            continue
+
+        cols.add(c)
+        pos.add(r + c)
+        neg.add(r - c)
+        board[r][c] = 1
+
+        backtrack(r+1, n, cols, pos, neg, board)
+
+        cols.remove(c)
+        pos.remove(r + c)
+        neg.remove(r - c)
+        board[r][c] = 0
+
+
+def nqueens(n):
+    """
+    Solution to nqueens problem
     Args:
-        n (int): The number of queens and the size of the chess board (n x n).
-
-    Returns:
-        list: A list of all possible solutions. Each solution is represented
-          as a list of integers,
-              where the value of i-th integer represents the column number of
-                the queen placed in the i-th row.
+        n (int): number of queens. Must be >= 4
+    Return:
+        List of lists representing coordinates of each
+        queen for all possible solutions
     """
+    cols = set()
+    pos_diag = set()
+    neg_diag = set()
+    board = [[0] * n for i in range(n)]
 
-    def can_place(pos, ocuppied_positions):
-        """
-        Check if a queen can be placed at the given position.
-
-        Args:
-            pos (int): The position to check.
-            ocuppied_positions (list): A list of positions already occupied
-              by other queens.
-
-        Returns:
-            bool: True if a queen can be placed at pos, False otherwise.
-        """
-        for i in range(len(ocuppied_positions)):
-            if (
-                ocuppied_positions[i] == pos
-                or ocuppied_positions[i] - i == pos - len(ocuppied_positions)
-                or ocuppied_positions[i] + i == pos + len(ocuppied_positions)
-            ):
-                return False
-        return True
-
-    def place_queens(n, index, ocuppied_positions, all_ocuppied_positions):
-        """
-        Place queens on the board using backtracking.
-
-        Args:
-            n (int): The number of queens and the size of the chess board
-              (n x n).
-            index (int): The current index (row number) to place a queen.
-            ocuppied_positions (list): A list of positions already occupied
-              by other queens.
-            all_ocuppied_positions (list): A list to store all possible
-              solutions.
-        """
-        if index == n:
-            all_ocuppied_positions.append(ocuppied_positions[:])
-            return
-
-        for i in range(n):
-            if can_place(i, ocuppied_positions):
-                ocuppied_positions.append(i)
-                place_queens(n, index + 1, ocuppied_positions,
-                             all_ocuppied_positions)
-                ocuppied_positions.pop()
-
-    all_ocuppied_positions = []
-    place_queens(n, 0, [], all_ocuppied_positions)
-    return all_ocuppied_positions
-
-
-def main():
-    """
-    The main function that checks the input and calls solve_nqueens
-      to find solutions.
-    """
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        exit(1)
-
-    try:
-        n = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
-        exit(1)
-
-    if n < 4:
-        print("N must be at least 4")
-        exit(1)
-
-    solutions = solve_nqueens(n)
-    for solution in solutions:
-        result = [[i, solution[i]] for i in range(n)]
-        print(result)
+    backtrack(0, n, cols, pos_diag, neg_diag, board)
 
 
 if __name__ == "__main__":
-    main()
+    n = sys.argv
+    if len(n) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        nn = int(n[1])
+        if nn < 4:
+            print("N must be at least 4")
+            sys.exit(1)
+        nqueens(nn)
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
