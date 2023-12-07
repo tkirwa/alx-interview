7e8f3443b3656dfb75383d5f20f97a5e3255f743
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-12-07 03:30:19 +0300

    add 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,57 +1,55 @@
 #!/usr/bin/python3
-"""Module defining isWinner function."""
+"""Prime game module.
+"""
 
 
-def isWinner(x, nums):
-    """Function to get who has won in prime game"""
-    mariaWinsCount = 0
-    benWinsCount = 0
-
-    for num in nums:
-        roundsSet = list(range(1, num + 1))
-        primesSet = primes_in_range(1, num)
-
-        if not primesSet:
-            benWinsCount += 1
-            continue
-
-        isMariaTurns = True
-
-        while True:
-            if not primesSet:
-                if isMariaTurns:
-                    benWinsCount += 1
-                else:
-                    mariaWinsCount += 1
-                break
-
-            smallestPrime = primesSet.pop(0)
-            roundsSet.remove(smallestPrime)
-
-            roundsSet = [x for x in roundsSet if x % smallestPrime != 0]
-
-            isMariaTurns = not isMariaTurns
-
-    if mariaWinsCount > benWinsCount:
-        return "Winner: Maria"
-
-    if mariaWinsCount < benWinsCount:
-        return "Winner: Ben"
-
-    return None
-
-
-def is_prime(n):
-    """Returns True if n is prime, else False."""
-    if n < 2:
+def is_prime(num):
+    """Check if a number is prime."""
+    if num < 2:
         return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
             return False
     return True
 
 
-def primes_in_range(start, end):
-    """Returns a list of prime numbers between start and end (inclusive)."""
-    primes = [n for n in range(start, end + 1) if is_prime(n)]
-    return primes
+def isWinner(x, nums):
+    """Determines the winner of a prime game session with `x` rounds."""
+    if x < 1 or not nums:
+        return None
+
+    def can_make_move(num, remaining_numbers):
+        """Check if a player can make a move in the game."""
+        return any(is_prime(
+            i) and i in remaining_numbers for i in range(2, num + 1))
+
+    marias_wins, bens_wins = 0, 0
+
+    for n in nums:
+        remaining_numbers = set(range(1, n + 1))
+        current_player = "Maria"
+
+        while remaining_numbers:
+            if not can_make_move(n, remaining_numbers):
+                break
+
+            valid_moves = [
+                i for i in range(2, n + 1) if is_prime(
+                    i) and i in remaining_numbers
+            ]
+            move = max(valid_moves)
+            remaining_numbers -= set(range(move, n + 1, move))
+            current_player = "Maria" if current_player == "Ben" else "Ben"
+
+        if current_player == "Maria":
+            marias_wins += 1
+        else:
+            bens_wins += 1
+
+    if marias_wins == bens_wins:
+        return None
+    return "Maria" if marias_wins > bens_wins else "Ben"
+
+
+if __name__ == "__main__":
+    print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
