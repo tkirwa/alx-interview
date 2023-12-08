71d659bdb48d37f8f6819f679662888113d81d93
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-12-08 22:51:45 +0300

    fix 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,55 +1,92 @@
 #!/usr/bin/python3
-"""Prime game module.
+"""
+0x0A. Prime Game
 """
 
 
-def is_prime(num):
-    """Check if a number is prime."""
-    if num < 2:
-        return False
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
-
-
 def isWinner(x, nums):
-    """Determines the winner of a prime game session with `x` rounds."""
-    if x < 1 or not nums:
-        return None
+    """
+    Determines the winner of a series of checkers games.
 
-    def can_make_move(num, remaining_numbers):
-        """Check if a player can make a move in the game."""
-        return any(is_prime(
-            i) and i in remaining_numbers for i in range(2, num + 1))
+    Args:
+    - x (int): The number of rounds.
+    - nums (list): An array of integers representing the range for each round.
 
-    marias_wins, bens_wins = 0, 0
+    Returns:
+    - str: The name of the player that won the most rounds.
+           If the winner cannot be determined, returns None.
+    """
+    def is_prime(num):
+        """
+        Checks if a number is prime.
 
-    for n in nums:
-        remaining_numbers = set(range(1, n + 1))
-        current_player = "Maria"
+        Args:
+        - num (int): The number to check.
 
-        while remaining_numbers:
-            if not can_make_move(n, remaining_numbers):
-                break
+        Returns:
+        - bool: True if the number is prime, False otherwise.
+        """
+        if num < 2:
+            return False
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
 
-            valid_moves = [
-                i for i in range(2, n + 1) if is_prime(
-                    i) and i in remaining_numbers
-            ]
-            move = max(valid_moves)
-            remaining_numbers -= set(range(move, n + 1, move))
-            current_player = "Maria" if current_player == "Ben" else "Ben"
+    def get_next_move(nums):
+        """
+        Finds the next prime number in the given list.
 
-        if current_player == "Maria":
-            marias_wins += 1
+        Args:
+        - nums (list): The list of integers to search.
+
+        Returns:
+        - int or None: The next prime number if found, None otherwise.
+        """
+        for num in nums:
+            if is_prime(num):
+                return num
+
+    def remove_multiples(nums, prime):
+        """
+        Removes multiples of a prime number from the list.
+
+        Args:
+        - nums (list): The list of integers to filter.
+        - prime (int): The prime number whose multiples should be removed.
+
+        Returns:
+        - list: The filtered list without multiples of the prime number.
+        """
+        return [num for num in nums if num % prime != 0]
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for round_num in range(x):
+        current_nums = list(range(1, nums[round_num] + 1))
+
+        while current_nums:
+            prime = get_next_move(current_nums)
+            if prime is None:
+                break  # No prime numbers left to choose
+
+            current_nums = remove_multiples(current_nums, prime)
+
+        # Determine the winner of the current round
+        if len(current_nums) % 2 == 0:
+            ben_wins += 1
         else:
-            bens_wins += 1
+            maria_wins += 1
 
-    if marias_wins == bens_wins:
-        return None
-    return "Maria" if marias_wins > bens_wins else "Ben"
+    # Determine the overall winner
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None  # No clear winner
 
 
-if __name__ == "__main__":
-    print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
+# Example usage
+print("Winner: {}".format(isWinner(3, [4, 5, 1])))
