bf2857295ffc39f377dc00247dcab6d9c73656af
Tonny Kirwa <118567738+tkirwa@users.noreply.github.com>
2023-12-09 07:42:37 +0300

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,92 +1,28 @@
 #!/usr/bin/python3
-"""
-0x0A. Prime Game
+""" Prime game module.
 """
 
 
 def isWinner(x, nums):
+    """ Determines the winner of a prime game session with `x` rounds.
     """
-    Determines the winner of a series of checkers games.
-
-    Args:
-    - x (int): The number of rounds.
-    - nums (list): An array of integers representing the range for each round.
-
-    Returns:
-    - str: The name of the player that won the most rounds.
-           If the winner cannot be determined, returns None.
-    """
-    def is_prime(num):
-        """
-        Checks if a number is prime.
-
-        Args:
-        - num (int): The number to check.
-
-        Returns:
-        - bool: True if the number is prime, False otherwise.
-        """
-        if num < 2:
-            return False
-        for i in range(2, int(num**0.5) + 1):
-            if num % i == 0:
-                return False
-        return True
-
-    def get_next_move(nums):
-        """
-        Finds the next prime number in the given list.
-
-        Args:
-        - nums (list): The list of integers to search.
-
-        Returns:
-        - int or None: The next prime number if found, None otherwise.
-        """
-        for num in nums:
-            if is_prime(num):
-                return num
-
-    def remove_multiples(nums, prime):
-        """
-        Removes multiples of a prime number from the list.
-
-        Args:
-        - nums (list): The list of integers to filter.
-        - prime (int): The prime number whose multiples should be removed.
-
-        Returns:
-        - list: The filtered list without multiples of the prime number.
-        """
-        return [num for num in nums if num % prime != 0]
-
-    maria_wins = 0
-    ben_wins = 0
-
-    for round_num in range(x):
-        current_nums = list(range(1, nums[round_num] + 1))
-
-        while current_nums:
-            prime = get_next_move(current_nums)
-            if prime is None:
-                break  # No prime numbers left to choose
-
-            current_nums = remove_multiples(current_nums, prime)
-
-        # Determine the winner of the current round
-        if len(current_nums) % 2 == 0:
-            ben_wins += 1
-        else:
-            maria_wins += 1
-
-    # Determine the overall winner
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
-        return None  # No clear winner
-
-
-# Example usage
-print("Winner: {}".format(isWinner(3, [4, 5, 1])))
+    if x < 1 or not nums:
+        return None
+    marias_wins, bens_wins = 0, 0
+    # generate primes with a limit of the maximum number in nums
+    n = max(nums)
+    primes = [True for _ in range(1, n + 1, 1)]
+    primes[0] = False
+    for i, is_prime in enumerate(primes, 1):
+        if i == 1 or not is_prime:
+            continue
+        for j in range(i + i, n + 1, i):
+            primes[j - 1] = False
+    # filter the number of primes less than n in nums for each round
+    for _, n in zip(range(x), nums):
+        primes_count = len(list(filter(lambda x: x, primes[0: n])))
+        bens_wins += primes_count % 2 == 0
+        marias_wins += primes_count % 2 == 1
+    if marias_wins == bens_wins:
+        return None
+    return 'Maria' if marias_wins > bens_wins else 'Ben'
